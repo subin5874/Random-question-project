f9a633654819ed9bae235c0a741256a5b2ba6f84
subin5874 <2004489@student.hywoman.ac.kr>
2024-11-11 17:23:15 +0900

    [Feat] 질문 추가/삭제 기능 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Home.module.css';
 
 function Home() {
+  interface question {
+    id: number;
+    content: string;
+  }
+
+  const [inputText, setInputText] = useState<string>('');
+
+  const [questionList, setQuestionList] = useState<question[]>([]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!inputText) {
+      return;
+    }
+    const newQuestion = { id: questionList.length + 1, content: inputText };
+    setQuestionList((prevQuestion) => [...prevQuestion, newQuestion]);
+    setInputText('');
+  };
+
+  const handleDelete = (id: number): void => {
+    setQuestionList((prevQuestionList) =>
+      prevQuestionList.filter((question) => question.id !== id)
+    );
+  };
+
+  const handleDeleteAll = () => {
+    if (questionList.length > 0) {
+      const isConfirmed = window.confirm('모든 질문을 삭제하나요?');
+      if (isConfirmed) {
+        setQuestionList([]);
+      } else {
+        return;
+      }
+    } else {
+      return;
+    }
+  };
+
   return (
     <div className={styles.main_container}>
       <header>
@@ -14,12 +56,14 @@ function Home() {
       </article>
       <article className={styles.question_container}>
         <section className={styles.question_form}>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className={styles.add_question_box}>
               <input
                 type="text"
+                value={inputText}
                 placeholder="질문을 작성하세요!"
                 className={styles.input_question}
+                onChange={handleInputChange}
               />
               <button type="submit" className={styles.add_question_btn}>
                 추가
@@ -30,42 +74,36 @@ function Home() {
         <section className={styles.question_list_container}>
           <div className={styles.question_list_topbar}>
             <span className={styles.list_title}>질문 리스트</span>
-            <button type="button" className={styles.delete_all_btn}>
+            <button
+              type="button"
+              onClick={() => handleDeleteAll()}
+              className={styles.delete_all_btn}
+            >
               모두 삭제
             </button>
           </div>
           <table>
             <tbody>
-              <tr className={styles.data_row}>
-                <td className={styles.data_question_cell}>
-                  <span className={styles.question_item}>질문 1</span>
-                </td>
-                <td className={styles.data_btn_cell}>
-                  <button type="button" className={styles.delete_question_btn}>
-                    삭제
-                  </button>
-                </td>
-              </tr>
-              <tr className={styles.data_row}>
-                <td className={styles.data_question_cell}>
-                  <span className={styles.question_item}>질문 2</span>
-                </td>
-                <td className={styles.data_btn_cell}>
-                  <button type="button" className={styles.delete_question_btn}>
-                    삭제
-                  </button>
-                </td>
-              </tr>
-              <tr className={styles.data_row}>
-                <td className={styles.data_question_cell}>
-                  <span className={styles.question_item}>질문 3</span>
-                </td>
-                <td className={styles.data_btn_cell}>
-                  <button type="button" className={styles.delete_question_btn}>
-                    삭제
-                  </button>
-                </td>
-              </tr>
+              {questionList.map((data, i) => {
+                return (
+                  <tr key={i} className={styles.data_row}>
+                    <td className={styles.data_question_cell}>
+                      <span className={styles.question_item}>
+                        {data.content}
+                      </span>
+                    </td>
+                    <td className={styles.data_btn_cell}>
+                      <button
+                        type="button"
+                        onClick={() => handleDelete(data.id)}
+                        className={styles.delete_question_btn}
+                      >
+                        삭제
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </section>
