6d63297c550b7b2f9855f54508e7afc990a522b3
subin5874 <2004489@student.hywoman.ac.kr>
2024-11-14 14:06:57 +0900

    [Feat] 랜덤 질문 선택 기능 구현

--- src/pages/RandomQuestionPage.tsx
+++ src/pages/RandomQuestionPage.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import styles from './RandomQuestionPage.module.css';
+import { Link } from 'react-router-dom';
 
 function RandomQuestionPage() {
-  return <div>RandomQuestionPage</div>;
+  const location = useLocation();
+  const questionList = location.state || {};
+  const [selectQuestion, setSelectQuestion] =
+    useState('버튼을 클릭하여 질문에 답해보세요');
+
+  const selectRandomQuestion = () => {
+    const randomIndex = Math.floor(Math.random() * questionList.length);
+    setSelectQuestion(questionList[randomIndex].content);
+  };
+
+  return (
+    <div className={styles.main_container}>
+      <header>
+        <Link to="/" className={styles.back_btn}>
+          &lt;돌아가기
+        </Link>
+        <h2 className={styles.main_title}>질문 돌림판</h2>
+      </header>
+      <article className={styles.randomQuestion_container}>
+        <div className={styles.content_box}>
+          <span className={styles.content_item}>{selectQuestion}</span>
+        </div>
+        <div className={styles.next_btn_box}>
+          <button
+            type="button"
+            className={styles.next_btn}
+            onClick={() => selectRandomQuestion()}
+          >
+            다음 질문
+          </button>
+        </div>
+      </article>
+    </div>
+  );
 }
 
 export default RandomQuestionPage;
